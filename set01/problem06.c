1194f55ebc5132d400136fbc981c7bcd6a7c5301
Shashank  khambhaita <113001856+Shashankkbdes21@users.noreply.github.com>
2022-09-18 08:55:42 +0000

    try problem06.c

---
+++ set01/problem06.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int largest(int a, int b)
+{
+  if (a>b){
+    return a;
+  }
+}
+if (a<b){
+  return b;
+}
+	else {
+		return a; 
+	}
+}
+
+int largest(int a, int b)
+{
+	if (a > b)
+	{
+		return a;
+	}
+	return b;
+}
+
+
+int largest(int a, int b)
+{
+	if (a > b)
+		return a;  /* if it is only one statement you do not need { }
+	return b;
+}
+
